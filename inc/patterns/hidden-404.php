df5ef68c28480a347b7846326c72dc8119238bc3
Caity Ross <internet@pandammonium.org>
2022-03-14 14:09:49 +0000

    Improves the escaping of the text.
    Uses methods for HTML visible on the front-end.

--- inc/patterns/hidden-404.php
+++ inc/patterns/hidden-404.php
@@ -1,5 +1,6 @@
 <?php
 /** 404 content.
+ *
  * @since 0.0.1
  * Moved to patterns in 0.0.4
  */
@@ -10,7 +11,7 @@ return array (
       <h1>' . esc_html__('Page not found', 'panda-puss') . '</h1>
       <!-- /wp:header -->
       <!-- wp:paragraph -->
-      <p>' . __('We\'ve looked in every cupboard and every room, but we can\'t seem to find that page anywhere.', 'panda-puss') . '</p>
+      <p>' . esc_html__('We\'ve looked in every cupboard and every room, but we can\'t seem to find that page anywhere.', 'panda-puss') . '</p>
       <!-- /wp:paragraph -->
-      <!-- wp:search {"label":"' . __('Try searching for it?', 'label', 'panda-puss') . '","buttonText":"Ok","className":"pp-search-404"} /-->'
+      <!-- wp:search {"label":"' . esc_html_x('Try searching for it?', 'label', 'panda-puss') . '","buttonText":"Ok","className":"pp-search-404"} /-->'
 );
