5ebf6ff3241a16790d12c25233f973508d3a7750
Caity Ross <internet@pandammonium.org>
2022-03-21 11:06:18 +0000

    Fixes spacing in class names.

--- inc/patterns/hidden-login-header.php
+++ inc/patterns/hidden-login-header.php
@@ -6,7 +6,7 @@
  */
 $log_in_out_link = '';
 $log_in_out_text = '';
-$log_in_out_class = ' pp-log-in_out';
+$log_in_out_class = 'pp-log-in_out';
 $log_in_out_class_in_out = '';
 if (is_user_logged_in()) {
   $log_in_out_link = wp_logout_url();
@@ -21,10 +21,10 @@ if (is_user_logged_in()) {
 $link = '<a class="' . $log_in_out_class_in_out . '" href="' . $log_in_out_link . '">' . $log_in_out_text . '</a>';
 
 $content = '
-  <!-- wp:group {"className":"pp-site-nav pp-header-nav' . $log_in_out_class . '"} -->
-  <div class="wp-block-group pp-site-nav pp-header-nav' . $log_in_out_class . '">
-    <!-- wp:paragraph {"className":"pp-site-nav-link pp-header-nav-link pp-block-navigation-item' . $log_in_out_class_in_out . '"} -->
-      <p class="pp-site-nav-link pp-header-nav-link pp-block-navigation-item' . $log_in_out_class_in_out . '">' . $link . '</p>
+  <!-- wp:group {"className":"pp-site-nav pp-header-nav ' . $log_in_out_class . '"} -->
+  <div class="wp-block-group pp-site-nav pp-header-nav ' . $log_in_out_class . '">
+    <!-- wp:paragraph {"className":"pp-site-nav-link pp-header-nav-link pp-block-navigation-item ' . $log_in_out_class_in_out . '"} -->
+      <p class="pp-site-nav-link pp-header-nav-link pp-block-navigation-item ' . $log_in_out_class_in_out . '">' . $link . '</p>
     <!-- /wp:paragraph -->
   </div>
   <!-- /wp:group -->';
