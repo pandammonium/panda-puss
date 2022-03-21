57257c2092188ed3cf9a7dec8bccf4fe76636dec
Caity Ross <internet@pandammonium.org>
2022-03-21 17:30:55 +0000

    Adds label for page title text.

--- inc/patterns/hidden-404.php
+++ inc/patterns/hidden-404.php
@@ -10,7 +10,7 @@ return array (
   'inserter'   => false,
   'content'    => '
     <!-- wp:header {"level":1} -->
-      <h1>' . esc_html__('Page not found', 'panda-puss') . '</h1>
+      <h1>' . esc_html_x('Page not found', 'page title', 'panda-puss') . '</h1>
     <!-- /wp:header -->
     <!-- wp:paragraph -->
     <p>' . esc_html__('We\'ve looked in every cupboard and every room, but we can\'t seem to find that page anywhere.', 'panda-puss') . '</p>
