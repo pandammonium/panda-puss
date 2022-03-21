da72ef12c0dafccf874d51692a83522fec90127b
Caity Ross <internet@pandammonium.org>
2022-03-21 11:05:54 +0000

    Fixes the display of the search label on 404 page.
    The WP showLabel option doesn't seem to work when set to false.

--- inc/patterns/hidden-404.php
+++ inc/patterns/hidden-404.php
@@ -15,5 +15,5 @@ return array (
     <!-- wp:paragraph -->
     <p>' . esc_html__('We\'ve looked in every cupboard and every room, but we can\'t seem to find that page anywhere.', 'panda-puss') . '</p>
     <!-- /wp:paragraph -->
-    <!-- wp:search {"label":"' . esc_html_x('Try searching for it?', 'label', 'panda-puss') . '","buttonText":"Ok","className":"pp-search-404"} /-->'
+    <!-- wp:search {"label":"' . esc_html_x('Try searching for it?', 'label', 'panda-puss') . '","buttonText":"Ok","showLabel":true,"className":"pp-search-404"} /-->'
 );
