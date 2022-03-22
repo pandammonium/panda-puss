b9098b5a06588fc410f84d785291a9a1135af534
Caity Ross <internet@pandammonium.org>
2022-03-22 18:15:16 +0000

    Moves the pagination into a pattern.

--- inc/patterns.php
+++ inc/patterns.php
@@ -63,6 +63,7 @@ if ( ! function_exists( 'panda_puss_register_patterns' ) ) :
       'general-login',
       'hidden-404',
       'hidden-login-header',
+      'pagination',
     );
     $patterns = apply_filters( 'panda_puss_patterns', $patterns );
 
