168e852d4c4560749227d2fe1df748a4a542ad90
Caity Ross <internet@pandammonium.org>
2022-05-26 14:09:08 +0100

    Changes button text to 'Sign up'.
    Adds missing quotation marks in properties.

--- inc/patterns/button-login.php
+++ inc/patterns/button-login.php
@@ -25,22 +25,22 @@ if ($user_is_logged_in) {
   $log_in_out_text = esc_html_x( 'Log in', 'log-in text', 'panda-puss' );
   $log_in_out_class_in_out = $log_in_out_class .' pp-log_in';
   $register_link = wp_registration_url();
-  $register_text = esc_html_x( 'Register', 'register text', 'panda-puss' );
-  $register_class = 'pp-register';
+  $register_text = esc_html_x( 'Sign up', 'register text', 'panda-puss' );
+  $register_class = ' pp-register';
   $register_anchor = '<a class="wp-block-button__link ' . $register_class . '" href="' . $register_link . '">' . $register_text . '</a>';
 }
 
 $log_in_out_anchor = '<a class="wp-block-button__link ' . $log_in_out_class_in_out . '" href="' . $log_in_out_link . '">' . $log_in_out_text . '</a>';
 
 $log_in_out_button = '
-<!-- wp:button {"className":"' . $log_in_out_class . ' "url":' . $log_in_out_link . '} -->
+<!-- wp:button {"className":"' . $log_in_out_class . '",url":"' . $log_in_out_link . '"} -->
 <div class="wp-block-button' . $log_in_out_class . '">
   ' . $log_in_out_anchor . '
 </div>
 <!-- /wp:button -->
 ';
 $register_button = '
-<!-- wp:button {"className":"' . $register_class . ' "url":' . $register_link . '} -->
+<!-- wp:button {"className":"' . $register_class . '",url":"' . $register_link . '"} -->
 <div class="wp-block-button' . $register_class . '">
   ' . $register_anchor . '
 </div>
